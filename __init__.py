4de41bd735f28e39b44ca6b538a509d32ce745c8
Benjamin Crom <benjamincrom@Benjamin-Croms-MacBook-Pro.local>
2018-01-01 11:50:11 -0500

    refactor

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from baseball.fetch_game import (get_game_from_url,
 								 get_game_xml_from_url,
 								 write_svg_from_url,
 								 write_svg_from_files,
+								 write_game_svg_and_html,
 								 get_game_generator_from_file_range,
 								 get_game_list_from_file_range,
 								 get_game_from_xml_strings,
-								 get_game_from_files,
-								 write_game_svg_and_html)
+								 get_game_from_files)
