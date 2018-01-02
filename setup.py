82f7939b33f5ab52e0e6c73d2976f0d4e192d97a
Benjamin Crom <benjamincrom@Benjamin-Croms-MacBook-Pro.local>
2018-01-01 22:47:47 -0500

    Fix docs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='baseball',
-      version='0.5',
+      version='0.6',
       description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
       url='https://github.com/benjamincrom/baseball',
       author='Benjamin B. Crom',
