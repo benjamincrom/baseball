3bba01d870f093ec295953d23251730ecf7634a4
Benjamin Crom <benjamincrom@Benjamin-Croms-MacBook-Pro.local>
2018-01-01 22:43:11 -0500

    Fix docs

--- baseball/__init__.py
+++ baseball/__init__.py
@@ -1,25 +1,25 @@
 from baseball.fetch_game import (get_game_from_url,
-								 get_game_xml_from_url,
-								 write_svg_from_url,
-								 write_svg_from_file_range,
-								 write_game_svg_and_html,
-								 get_game_generator_from_file_range,
-								 get_game_list_from_file_range,
-								 get_game_from_xml_strings,
-								 get_game_from_files,
-								 get_filename_list)
+                                 get_game_xml_from_url,
+                                 write_svg_from_url,
+                                 write_svg_from_file_range,
+                                 write_game_svg_and_html,
+                                 get_game_generator_from_file_range,
+                                 get_game_list_from_file_range,
+                                 get_game_from_xml_strings,
+                                 get_game_from_files,
+                                 get_filename_list)
 
 from baseball.process_game_xml import MLB_TEAM_CODE_DICT
 
 from baseball.baseball import (PlayerAppearance,
-							   Player,
-							   Team,
-							   Game,
-							   Inning,
-							   PlateAppearance)
+                               Player,
+                               Team,
+                               Game,
+                               Inning,
+                               PlateAppearance)
 
 from baseball.baseball_events import (Substitution,
-									  Switch,
-									  Pitch,
-									  Pickoff,
-									  RunnerAdvance)
+                                      Switch,
+                                      Pitch,
+                                      Pickoff,
+                                      RunnerAdvance)
