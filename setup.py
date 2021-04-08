2212ac3b9f356b22ef0f94947541d27fb6938790
benjamincrom@gmail.com <benjamincrom@gmail.com>
2021-04-08 10:40:35 -0400

    Coming right along

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='baseball',
-      version='7.1',
+      version='7.2',
       description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
       url='https://github.com/benjamincrom/baseball',
       author='Benjamin B. Crom',
