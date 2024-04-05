f7eae862371d0425655e9db7110b3436ff527d0c
benjamincrom@gmail.com <benjamincrom@gmail.com>
2024-04-05 13:25:51 -0400

    Fixed datadog synthetics link

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='baseball',
-      version='16.24',
+      version='16.25',
       description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
       url='https://github.com/benjamincrom/baseball',
       author='Benjamin B. Crom',
