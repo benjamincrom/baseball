db39c7bf3e96cc40d8fe5bb14a9a6670e7baa78b
benjamincrom@gmail.com <benjamincrom@gmail.com>
2024-05-22 14:40:22 -0400

    Fix pitcher sub lines with CS

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='baseball',
-      version='16.54',
+      version='16.55',
       description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
       url='https://github.com/benjamincrom/baseball',
       author='Benjamin B. Crom',
