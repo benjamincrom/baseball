f321b1134df974bc470cf591a7360e32cc6fabf1
benjamincrom@gmail.com <benjamincrom@gmail.com>
2024-05-25 22:44:00 -0400

    Delay feature

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='baseball',
-      version='16.71',
+      version='16.72',
       description='Library to download, anaylyze, and visualize events in Major League Baseball games.',
       url='https://github.com/benjamincrom/baseball',
       author='Benjamin B. Crom',
